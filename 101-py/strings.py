c252c3947a9e8584a8d6c2992181fb87056cb82a
HipsterSantos <santoscampos269@gmail.com>
2021-04-28 06:39:42 +0100

    updating string one oh one

--- 101-py/strings.py
+++ 101-py/strings.py
@@ -1,5 +1,8 @@
-"""Unicode and scape sequences"""
+"""Unicode and scape sequences ASCII representation"""
 
 print("\333 this octal number")
 print("\x3434333 this is decimal number")
 print("\b3333 this a binary one")
+
+unicode_a = '\N{LATIN SMALL LETTER A}'
+print(unicode_a)
