fb546e709d6ddc8fce2afa1b32f37c06eb589276
HipsterSantos <santoscampos269@gmail.com>
2021-06-14 00:41:17 +0100

    updating comprehensions

--- 101-py/comprehensions.py
+++ 101-py/comprehensions.py
@@ -3,5 +3,13 @@ x = [ i for i in range(4)]
 print(x)
 b = [ c for c in list(x)]
 print(b)
-if [i for  i in list(b) if i>3 in i ]:
+if [i for  i in list(b) if i > 3 in i ]:
     print("yes got it ")
+x = ['1','2','3','5']
+y = [int(i) for i in x]
+print(y)
+
+
+vec = [[1,2,3],[4,5,6],[7,8,9]]
+result = [num for elem in vec for num in elem]
+print(result)
