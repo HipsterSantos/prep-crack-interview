67ce5a960a2eab428e5cb9dad9ed315c9bc090a5
HipsterSantos <santoscampos269@gmail.com>
2021-06-14 01:00:56 +0100

    tuples

--- 101-py/dictionary.py
+++ 101-py/dictionary.py
@@ -3,4 +3,8 @@ monkey = {
     "name":"Jessica",
     "address":"Angola"
 }
-print(monkey)
+print(monkey['name'])
+#check whether name exists or not 
+print("  name  ".strip() in monkey)
+ned =dict( list(monkey.keys()) )
+print(ned)
