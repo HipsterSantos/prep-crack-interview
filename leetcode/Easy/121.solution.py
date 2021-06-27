e5b4016602b028f58f45398bc713bbedfe551904
HipsterSantos <santoscampos269@gmail.com>
2021-06-27 18:36:35 +0100

    first attempt to the solution

--- leetcode/Easy/121.solution.py
+++ leetcode/Easy/121.solution.py
@@ -5,3 +5,14 @@ class Solution:
     def selStock(self,prices:List[int]):
         for index, value in enumerate(prices):
             self.prices += val - prices[index+1]
+
+    def buyStock(self, prices:List[int]):
+        moving  = True
+        profit =  list()
+        while(moving):
+            for ind, val in enumerate(prices):
+                for a in range(ind+1,(len(prices) )-1 ):
+                    profit.add(prices[a] - val)
+                    if(a == len(prices)):
+                        moving = False
+                return profit
