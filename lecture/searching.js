83644d7b55ccd9e727b6c3f05e10470966034289
HipsterSantos <santoscampos269@gmail.com>
2021-09-19 06:31:51 +0100

    binarySearch

--- lecture/searching.js
+++ lecture/searching.js
@@ -1,12 +1,38 @@
 //linear search 
-
-
-
-
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} n 
+ * @returns true
+ * 
+ * Time complexity: O(n) */
 function linearSearch(arr,n){
     for (let i = 0 ; i<arr.length-1;i++ ){
         if(arr[i] == n){
             return true;
         }
     }
+}
+
+/**
+ * Binary Search
+ */
+
+function binarySearch(arr,n){
+    let low = 0;
+    let high = arr.length-1;
+
+    while(low <= high){
+        let mid = Math.floor((low+high)/2);
+        if(arr[mid] == n){
+            return true;
+        }
+        else if(arr[mid] < n){
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return false;               
 }
