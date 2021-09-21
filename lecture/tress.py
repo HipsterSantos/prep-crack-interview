2267c8ab788747d6b00b125d19e60e995bb1dce1
hipster <santoscampos269@gmail.com>
2021-09-21 05:55:33 +0100

    pre-order-traverse helper added

--- lecture/tress.py
+++ lecture/tress.py
@@ -1,3 +1,14 @@
+class BinaryTree:
+    def __init__(self):
+        self._root = null
+    def traversePreOrderHelper(node:Node):
+        traversePreOrderHelper(self._root)
+        if not node :
+            return
+        print(node.data)
+        traversePreOrderHelper(node.left)
+        traversePreOrserHelper(node.right)
+
 class Node:
     def __init__(self,data):
         self.data = data 
