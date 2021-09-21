9cb5f802bf505a793374c10a282618499a4380d5
hipster <santoscampos269@gmail.com>
2021-09-21 06:03:22 +0100

    in-Order-traverse helper added

--- lecture/tress.py
+++ lecture/tress.py
@@ -1,13 +1,37 @@
 class BinaryTree:
     def __init__(self):
         self._root = null
-    def traversePreOrderHelper(node:Node):
+    def traversePreOrderHelper(self,node:Node):
+        """root-> left-> right"""
         traversePreOrderHelper(self._root)
         if not node :
             return
         print(node.data)
         traversePreOrderHelper(node.left)
         traversePreOrserHelper(node.right)
+    #implementing this version interactively 
+    def traversePreOrderIntertive(self):
+        var nodeStack = []
+        nodeStack.append(self._root)
+        """
+        Logic for this solution 
+        pop all itmes obe by one. Do following for every popper item 
+        -print it 
+        push its right child 
+        push its lfef child 
+
+        """
+        pass 
+    def traverseInOrderHelper(self,node):
+        traverseInOrderHelper(self._root)
+        """
+        left->root->right
+        """
+        if not node:
+            return 
+        traverseInOrderHelper(node.left)
+        print(node.data)
+        traveseInOrderHelper(node.right)
 
 class Node:
     def __init__(self,data):
